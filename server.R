d1c804b9ef57a158bbc5b72712c236e09974eb0c
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2023-09-25 16:03:34 -0600

    Mejoras y mas funcionalidades

--- server.R
+++ server.R
@@ -3,8 +3,7 @@ library(DT)
 library(ggplot2)
 library(plotly)
 library(wordcloud2)
-library(leaflet)
-library(ggmap)
+library(dplyr)
 
 function(input, output, session) {
   my_data <- read.csv("movies.csv")
@@ -24,7 +23,7 @@ function(input, output, session) {
   top_3_genres <- head(sort(genre_freq, decreasing = TRUE), 3)
   pie_data <- data.frame(Genre = names(top_3_genres), Frequency = as.vector(top_3_genres))
   output$genre_pie_chart <- renderPlot({
-    pie(pie_data$Frequency, labels = pie_data$Genre, main = "Top 3 Genres")
+    pie(pie_data$Frequency, labels = pie_data$Genre, main = "3 Géneros de Peliculas más Populares")
   })
   
   ratings_distribution <- reactive({
@@ -58,7 +57,121 @@ function(input, output, session) {
     )
   })
   
+  movie_info <- reactive({
+    movie_name <- input$movie_name
+    movie <- my_data[my_data$Title == movie_name, ]
+    
+    if (nrow(movie) == 0) {
+      return(NULL)
+    } else {
+      info_text <- paste(
+        "Title:", movie$Title[1], "<br>",
+        "Director:", paste0(movie$Director[1]), "<br>",
+        "Duration:", paste0(movie$Runtime[1], " minutes"), "<br>",
+        "Rating:", paste0(movie$Rating[1]), "<br>",
+        "Stars:", paste0(movie$Stars[1]), "<br>",
+        "Genre:", paste0(movie$Genre[1])
+      )
+      
+      return(HTML(info_text))
+    }
+  })
   
+  output$movie_info_box <- renderValueBox({
+    info <- movie_info()
+    
+    if (!is.null(info)) {
+      infoBox(
+        info,
+        "Movie Information",
+        icon = icon("film"),
+        color = "blue", 
+        fill = TRUE
+      )
+    } else {
+      infoBox(
+        "Movie not found",
+        "Movie Information",
+        icon = icon("film"),
+        color = "teal",
+        fill = TRUE
+        
+      )
+    }
+  })
+  
+  
+  data_filtered <- my_data %>%
+    filter(!is.na(Month) & Month != "Unknown" & Month != "")
+  
+  month_counts <- table(data_filtered$Month)
+  
+  top_months <- names(sort(month_counts, decreasing = TRUE)[1:5])
+  
+  top_month <- names(sort(month_counts, decreasing = TRUE)[1])
+  
+  data_top_months <- data_filtered %>%
+    filter(Month %in% top_months)
+  
+  data_top_month <- data_filtered %>%
+    filter(Month == top_month)
+  
+  summarized_data <- data_top_months %>%
+    group_by(Month) %>%
+    summarize(Count = n())
+  
+  
+  output$pie_chart <- renderPlot({
+    pie_chart <- ggplot(summarized_data, aes(x = "", y = Count, fill = Month)) +
+      geom_bar(stat = "identity") +
+      coord_polar(theta = "y") +
+      theme_void() +
+      labs(title = "Meses en los que se estrenan más peliculas")
+    
+    print(pie_chart)
+  })
+  
+  output$top_month_box <- renderValueBox({
+    valueBox(
+      top_month,
+      subtitle = "Top Month",
+      icon = icon("calendar"),
+      color = "blue"
+    )
+  })
+  
+  
+  filtered_data <- my_data %>%
+    filter(!is.na(Year) & Year != "Unknown" & Year != "")
+  
+  year_counts <- table(filtered_data$Year)
+  
+  top_year <- names(sort(year_counts, decreasing = TRUE)[1])
+  
+  data_top_year <- filtered_data %>%
+    filter(Year == top_year)
+  
+  
+  output$top_year_box <- renderValueBox({
+    valueBox(
+      top_year,
+      subtitle = "Top Year",
+      icon = icon("calendar"),
+      color = "purple"
+    )
+  })
+  
+
+  average_rating <- mean(my_data$Rating, na.rm = TRUE)
+  
+  output$average_rating_box <- renderValueBox({
+    valueBox(
+      round(average_rating, 2),  # Round to 2 decimal places
+      "Average Rating",
+      icon = icon("star"),
+      color = "green"
+    )
+  })
   
   
 }
--- ui.R
+++ ui.R
@@ -4,24 +4,57 @@ library(ggplot2)
 library(plotly)
 library(leaflet)
 library(wordcloud2)
-library(ggmap)
+library(dplyr)
+library(jsonlite)
+library(shinydashboard)
 
-fluidPage(
-  titlePanel("Lab 4: Popular Movies"),
-  tabsetPanel(
-    tabPanel("Movie Charts", 
-             fluidRow(
-               column(6, plotOutput("genre_pie_chart"))
-             ),
-             plotOutput("ratings_bar_chart")
-             
-    ),
-    tabPanel("Most Popular Directors",
-             wordcloud2Output("wordcloud")),
-    
-    tabPanel("Data Table", 
-             DTOutput("data_table")
+# Define UI
+dashboardPage(
+  dashboardHeader(title = "Lab 4: Popular Movies"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Movie Charts", tabName = "movie_charts"),
+      menuItem("Most Popular Directors", tabName = "popular_directors"),
+      menuItem("Search for a Movie", tabName = "search_movie"),
+      menuItem("Data Table", tabName = "data_table")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "movie_charts",
+        fluidRow(
+          div(style = "display: flex; justify-content: space-between;",
+              plotOutput("genre_pie_chart", width = "48%"),
+              plotOutput("pie_chart", width = "50%")
+          ),
+          valueBoxOutput("top_year_box"),
+          valueBoxOutput("top_month_box"),
+          valueBoxOutput("average_rating_box"),
+          plotOutput("ratings_bar_chart")
+        )
+      ),
+      tabItem(
+        tabName = "popular_directors",
+        wordcloud2Output("wordcloud")
+      ),
+      tabItem(
+        tabName = "search_movie",
+        fluidRow(
+          textInput("movie_name", "Movie name:"),
+          actionButton("submit", "Submit")
+        ),
+        fluidRow(
+          tags$br(),
+          valueBoxOutput("movie_info_box")
+        )
+      ),
+      tabItem(
+        tabName = "data_table",
+        DTOutput("data_table")
+      )
     )
   )
 )
+
 
