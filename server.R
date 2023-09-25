842ea5d0115bd68252b127b7487eeeec64c48bab
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2023-09-25 14:57:59 -0600

    Más graficas y word charts

--- server.R
+++ server.R
@@ -1,9 +1,14 @@
 library(shiny)
 library(DT)
-
+library(ggplot2)
+library(plotly)
+library(wordcloud2)
+library(leaflet)
+library(ggmap)
 
 function(input, output, session) {
   my_data <- read.csv("movies.csv")
+  
   
   output$data_table <- DT::renderDataTable({
     print("Rendering data")
@@ -22,5 +27,39 @@ function(input, output, session) {
     pie(pie_data$Frequency, labels = pie_data$Genre, main = "Top 3 Genres")
   })
   
-  }
+  ratings_distribution <- reactive({
+    ggplot(data = my_data, aes(x = Rating)) +
+      geom_bar(fill = "blue", color = "black") +
+      labs(title = "Ratings de Peliculas", x = "Rating", y = "Count") +
+      theme_minimal()
+  })
+  
+  output$ratings_bar_chart <- renderPlot({
+    ratings_distribution()
+  })
+  
+  director_counts <- table(my_data$Director)
+  director_data <- data.frame(Director = names(director_counts), Count = as.vector(director_counts))
+  
+  
+  
+  output$wordcloud <- renderWordcloud2({
+    # Prepare data for the word cloud
+    director_wordcloud_data <- data.frame(
+      words = director_data$Director,
+      freq = director_data$Count
+    )
+    
+    wordcloud2(
+      director_wordcloud_data,
+      color = "random-dark",
+      backgroundColor = "white",
+      size = 1.5
+    )
+  })
+  
+  
+  
+  
+}
 
--- ui.R
+++ ui.R
@@ -1,16 +1,27 @@
 library(shiny)
 library(DT)
+library(ggplot2)
+library(plotly)
+library(leaflet)
+library(wordcloud2)
+library(ggmap)
 
 fluidPage(
   titlePanel("Lab 4: Popular Movies"),
   tabsetPanel(
     tabPanel("Movie Charts", 
-             plotOutput("genre_pie_chart")
+             fluidRow(
+               column(6, plotOutput("genre_pie_chart"))
+             ),
+             plotOutput("ratings_bar_chart")
+             
     ),
+    tabPanel("Most Popular Directors",
+             wordcloud2Output("wordcloud")),
+    
     tabPanel("Data Table", 
              DTOutput("data_table")
     )
   )
 )
-
 
